4ea22d9afa42d00800c631fa88247447963290cc
bluesmarf777 <junek02026@gmail.com>
2025-05-11 23:29:27 +0900

    Update alerts.ts

--- pages/api/alerts.ts
+++ pages/api/alerts.ts
@@ -8,7 +8,7 @@ type CoinData = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = await fetch(https://stockrader-backend.onrender.com);
+  const response = await fetch("https://stockrader-backend.onrender.com/api/coin-alerts");
   const data = await response.json();
   res.status(200).json(data);
 }
